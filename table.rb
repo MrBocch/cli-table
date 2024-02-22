3d0cc5004153df88b45252a41afc592d05391d20
jorge <jorge@jorges-MacBook-Air.local>
2024-02-21 20:24:06 -0600

    officially got it working

--- table.rb
+++ table.rb
@@ -10,9 +10,9 @@ class Table
 
   def printTable
     # i should probably check for things 
-    if @data.empty?
-      raise "Where is the data?"
-    end
+    #if @data.empty?
+    #  raise "Where is the data?"§
+    #end
     sdata = [@header.clone]
     sdata += self.datato_s
     # does everything have to be turned into a string?
@@ -34,20 +34,40 @@ class Table
     tsign  = {:ut => '┴', :dt => '┬', :rt => '├', :lt => '┤', :cross => '┼'}
     corner = {:tr => '┌', :tl => '┐', :br => '┘', :bl => '└'}
 
-    # how do print lower and top part of table?
-    # ┌────┬──────┬───┐
-    # |data| data | x | 
-    # ├────┼──────┼───┤
+    top = corner[:tr]
+    mid = tsign[:rt]
+    bot = corner[:bl]
 
+    head = "#{wall}"
+    (0...sdata[0].length).each do |i|
+      temp = " #{sdata[0][i].ljust(rpad[i])}#{wall}"
+      top += temp.split("").map{|s| floor}.join("")
+      top[-1] = tsign[:dt]
+
+      mid += temp.split("").map{|s| floor}.join("")
+      mid[-1] = tsign[:cross]
+
+      bot += temp.split("").map{|s| floor}.join("")
+      bot[-1] = tsign[:ut]
+      head << temp
+    end
+    top[-1] = corner[:tl]
+    mid[-1] = tsign[:lt]
+    bot[-1] = corner[:br]
+
+    puts top 
+    puts head
+    puts mid
     # we can start printing table 
-    sdata.each do |l|
+    sdata[1..-1].each do |l|
       (0...l.length()).each do |i|
         print("#{wall} #{l[i].ljust(rpad[i])}")
       end
       print "#{wall}"
       puts ""
     end
- 
+
+    puts bot 
   end
 
   # is this a bad idea
@@ -60,85 +80,4 @@ class Table
     end
     stringify
   end
-end
-
-
-# this monstrosity
-# is the code for making table
-def stats()
-  # rows = [[data], [data], [data]]
-
-  # here we convert 
-  # everything into a 
-  # string, for convinience?
-
-
-  # if they were not strings we could not check for this
-  # we get the longest membe rin column, for padding porpuses
-  # we want to pad up shorter strings
-
-  id = "id".ljust(rpad_id, " ")
-  activity = "Activity".ljust(rpad_act, " ")
-  time = "Time".ljust(rpad_time, " ")
- 
-  floor = '─'
-  wall = '│' 
-
-  id_col = "#{wall} #{id} #{wall}"
-  activity_col = "#{activity} #{wall}"
-  time_col = "#{time} #{wall}"
-
-  tsign  = {:ut => '┴', :dt => '┬', :rt => '├', :lt => '┤', :cross => '┼'}
-  corner = {:tr => '┌', :tl => '┐', :br => '┘', :bl => '└'}
-  # this must be the worst code i have ever written in my life
-  # sorrow to those that lay their eyes upon it
-  uheader = (id_col).split("").map{|c| floor}.join("")
-  uheader[0] = corner[:tr]
-  uheader[-1] = corner[:tl]
-
-  uheader += (activity_col).split("").map{|c| floor}.join("")
-  uheader[id_col.length-1] = tsign[:dt]
-  uheader += tsign[:dt]
-
-  uheader += (time_col).split("").map{|c| floor}.join("")
-  uheader += corner[:tl]
-
-  dheader = (id_col).split("").map{|c| floor}.join("")
-  dheader[0] = tsign[:rt]
-  dheader[-1] = tsign[:cross]
-
-  dheader += (activity_col).split("").map{|c| floor}.join("")
-  dheader += tsign[:cross]
-  
-  dheader += (time_col).split("").map{|c| floor}.join("")
-  dheader += tsign[:lt]
-
-  endtable = (id_col).split("").map{|c| floor}.join("")
-  endtable[0] = corner[:bl]
-  endtable[-1] = tsign[:ut]
-
-  endtable += (activity_col).split("").map{|c| floor}.join("")
-  endtable += tsign[:ut]
-
-  endtable += (time_col).split("").map{|c| floor}.join("")
-  endtable += corner[:br]
-
-  header = "#{id_col} #{activity_col} #{time_col}" 
-
-  # print the top part of table 
-  puts uheader 
-  puts header
-  puts dheader
-  # print part of table right below header
-
-  rows.each do |row|
-    id = row[0].to_s.ljust(rpad_id, " ")
-    act = row[1].ljust(rpad_act, " ") # so cool
-    time = row[2].ljust(rpad_time, " ")
-    # h, m = secToHM(row[2])
-
-    puts "#{wall} #{id} #{wall} #{act} #{wall} #{time} #{wall}"
-  end
-  puts endtable
-
 end
